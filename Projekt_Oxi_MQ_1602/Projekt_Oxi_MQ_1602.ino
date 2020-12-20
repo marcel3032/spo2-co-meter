de93c771cb58fed319ca895cd1e2668703bf47fd
Marcel Palaj <palaj.marcel@gmail.com>
2020-12-20 11:17:13 +0100

    constants update +- reliable peak detection

--- Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
+++ Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
@@ -11,7 +11,7 @@ const int max_measurments_size=50, min_inserted_finger=200;
 float RED_measurments[max_measurments_size], IR_measurments[max_measurments_size];
 int index=0, measurments_size=0;
 float sum_bpms=0, last_peak=0;
-const int max_bpms=5, avg_const=3, diff=0.3, min_diff_between_peak=200;
+const int max_bpms=5, avg_const=4, diff=0.3, min_diff_between_peak=200;
 int bpms=0, index_bpm=0;
 float bpm[max_bpms]={0,0,0,0,0};
 float REDmin=30000, REDmax=0, IRmin=30000, IRmax=0, R=0;
@@ -78,16 +78,12 @@ void write_measurment(float IR, float RED)
 bool is_peak()
 {
   float now =avg_ago(index);
-  float prev=avg_ago(index-3);
-  float last=avg_ago(index-5);
-  //Serial.print(now);
-  //Serial.print(",");
-  if(last<prev && last>now && last+diff<=prev && prev>=now+diff)
-  {
-    Serial.print(100);
+  float prev=avg_ago(index-1);
+  float prevprev=avg_ago(index-2);
+  float last=avg_ago(index-4);
+  
+  if(last+diff<prevprev && prevprev+diff<prev && prev>now+diff)
     return true;
-  }
-  Serial.print(0);
   return false;
 }
 
@@ -107,6 +103,7 @@ float compute_BPM()
   float avg=0;
   if(is_peak() && millis()- last_peak>min_diff_between_peak)
   {
+    Serial.print(100);
     sum_bpms-=bpm[index_bpm];
     sum_bpms+=bpm[index_bpm]=millis()- last_peak;
     last_peak=millis();
@@ -115,10 +112,12 @@ float compute_BPM()
     R=(((REDmax-REDmin)*IRmin) / ((IRmax-IRmin)*REDmin));
     REDmin=30000, REDmax=0, IRmin=30000, IRmax=0;
   }
+  else
+    Serial.print(0);
   for(int i=0;i<max_bpms;i++)
-    {
-      avg+=bpm[i];
-    }
+  {
+    avg+=bpm[i];
+  }
   Serial.print(",");
   Serial.print(R);
   Serial.print(",");
