5fc897711523484808f001b848c61d4c2c41e2e2
Marcel Palaj <palaj.marcel@gmail.com>
2020-12-22 16:33:55 +0100

    Start coding LCD

--- Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
+++ Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
@@ -1,21 +1,40 @@
+#include <LiquidCrystal.h>
+
+//constants and global variables
+  //LCD
+    const int rs = 12, en = 7, d4 = 5, d5 = 4, d6 = 8, d7 = 2;
+    const int lcd_red=3, lcd_green=11, lcd_blue=6;
+    const int n_r=123, n_g=123, n_b=123;
+    const int o_r=0, o_g=255, o_b=0;
+    const int c_r=255, c_g=123, c_b=0;
+    const int w_r=255, w_g=0, w_b=0;
+    enum farba {neutral, ok, caution, warning};
+    LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  //MQ7
+    int MQ7_output = A1, sensorValue = 0, MQ7_control=9;
+  //IR and RED measurment
+    int T=20, sensorPin=A0, IR_LED=10, RED_LED=13;
+    const int max_measurments_size=50, min_inserted_finger=200;
+    float RED_measurments[max_measurments_size], IR_measurments[max_measurments_size];
+    int index=0, measurments_size=0;
+  //BPM
+    float sum_bpms=0, last_peak=0;
+    const int max_bpms=5, avg_const=4, diff=0.23, min_diff_between_peak=300;
+    int bpms=0, index_bpm=0;
+    float bpm[max_bpms];
+  //R and SPO2
+    float REDmin=30000, REDmax=0, IRmin=30000, IRmax=0, R=0, sum_Rs=0;
+    float Rs[max_bpms];
+//end constants and global variables
+
 float measure(int light_port, int input_port);
 void write_measurment(float IR, float RED);
 float compute_BPM();
 bool is_peak();
 float avg_ago(int index);
 void change_max_min_for_R(float RED, float IR);
-
-//constants and global variables
-int T=20, sensorPin=A0, IR_LED=10, RED_LED=13;
-const int max_measurments_size=50, min_inserted_finger=200;
-float RED_measurments[max_measurments_size], IR_measurments[max_measurments_size];
-int index=0, measurments_size=0;
-float sum_bpms=0, last_peak=0;
-const int max_bpms=5, avg_const=4, diff=0.3, min_diff_between_peak=200;
-int bpms=0, index_bpm=0;
-float bpm[max_bpms]={0,0,0,0,0};
-float REDmin=30000, REDmax=0, IRmin=30000, IRmax=0, R=0;
-//end constants and global variables
+void lcd_backlight(int r, int g, int b);
+void lcd_backlight(enum farba);
 
 void setup() {
   Serial.begin(9600);
@@ -25,9 +44,23 @@ void setup() {
   pinMode(sensorPin,INPUT);
   pinMode(RED_LED,OUTPUT);
   pinMode(IR_LED,OUTPUT);
+  
+  pinMode(MQ7_control, OUTPUT);
 
+  pinMode(en, OUTPUT);
+  pinMode(lcd_red, OUTPUT);
+  pinMode(lcd_green, OUTPUT);
+  pinMode(lcd_blue, OUTPUT);
+
+  lcd_backlight(neutral);
+  
   digitalWrite(RED_LED,LOW);
   digitalWrite(IR_LED,LOW);  
+
+  lcd.begin(16, 2);
+
+  for(int i=0;i<max_bpms;i++)
+    bpm[i]=Rs[i]=0;
 }
 
 void loop() {
@@ -93,41 +126,45 @@ float avg_ago(int index)
   for(int i=avg_const-1;i>=0;i--)
   {
     res+=RED_measurments[(index-i+max_measurments_size)%max_measurments_size];
-    //res+=IR_measurments[(index-i+max_measurments_size)%max_measurments_size];
+    res+=IR_measurments[(index-i+max_measurments_size)%max_measurments_size];
   }
-  return res/(avg_const);
+  return res/(2*avg_const);
 }
 
 float compute_BPM()
 {
-  float avg=0;
   if(is_peak() && millis()- last_peak>min_diff_between_peak)
   {
     Serial.print(100);
     sum_bpms-=bpm[index_bpm];
-    sum_bpms+=bpm[index_bpm]=millis()- last_peak;
+    sum_Rs-=Rs[index_bpm];
+    
+    sum_bpms+=(bpm[index_bpm]=millis()- last_peak);
+    sum_Rs+=(Rs[index_bpm] = (((REDmax-REDmin)*IRmin) / ((IRmax-IRmin)*REDmin)));
+
+    REDmin=30000, REDmax=0, IRmin=30000, IRmax=0;    
     last_peak=millis();
+    
     index_bpm++;
     index_bpm%=max_bpms; 
-    R=(((REDmax-REDmin)*IRmin) / ((IRmax-IRmin)*REDmin));
-    REDmin=30000, REDmax=0, IRmin=30000, IRmax=0;
   }
   else
     Serial.print(0);
-  for(int i=0;i<max_bpms;i++)
-  {
-    avg+=bpm[i];
-  }
+
+  if(sum_Rs/max_bpms>=92)
+    lcd_backlight(ok);
+  if(sum_Rs/max_bpms<=88)
+    lcd_backlight(warning);
+  else if(sum_Rs/max_bpms<92)
+    lcd_backlight(caution);
+
+
   Serial.print(",");
-  Serial.print(R);
+  Serial.print(sum_Rs/max_bpms);
   Serial.print(",");
-  Serial.print(-19*R+112);
+  Serial.print(-19*(sum_Rs/max_bpms)+112);
   Serial.print(",");
-  Serial.print(60000/(avg/max_bpms));
-  /*Serial.print(",");
-  Serial.print((RED_measurments[index]-850)*5);
-  Serial.print(",");
-  Serial.print((IR_measurments[index]-850)*5);*/
+  Serial.print(60000/(sum_bpms/max_bpms));
 }
 
 void change_max_min_for_R(float RED, float IR)
@@ -136,4 +173,22 @@ void change_max_min_for_R(float RED, float IR)
   REDmin=min(RED, REDmin);
   IRmax=max(IR, IRmax);
   IRmin=min(IR, IRmin);
+}
+
+void lcd_backlight(int r, int g, int b)
+{
+  analogWrite(lcd_red, r);
+  analogWrite(lcd_green, g);
+  analogWrite(lcd_blue, b);
+}
+void lcd_backlight(farba f)
+{
+  if(f==neutral)
+    lcd_backlight(n_r, n_g, n_b);
+  if(f==ok)
+    lcd_backlight(o_r, o_g, o_b);
+  if(f==caution)
+    lcd_backlight(c_r, c_g, c_b);
+  if(f==warning)
+    lcd_backlight(w_r, w_g, w_b);
 }
