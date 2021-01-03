0d82d2583c9073ebafbf06744af7be6682b4e53c
Marcel Palaj <palaj.marcel@gmail.com>
2021-01-03 16:41:25 +0100

    Add mq7 and LCD stuff

--- Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
+++ Projekt_Oxi_MQ_1602/Projekt_Oxi_MQ_1602.ino
@@ -4,14 +4,20 @@
   //LCD
     const int rs = 12, en = 7, d4 = 5, d5 = 4, d6 = 8, d7 = 2;
     const int lcd_red=3, lcd_green=11, lcd_blue=6;
-    const int n_r=123, n_g=123, n_b=123;
+    const int n_r=110, n_g=125, n_b=125;
     const int o_r=0, o_g=255, o_b=0;
     const int c_r=255, c_g=123, c_b=0;
     const int w_r=255, w_g=0, w_b=0;
-    enum farba {neutral, ok, caution, warning};
+    enum farba {neutral=0, ok=1, caution=2, warning=3};
+    farba current_color=caution;
     LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
   //MQ7
-    int MQ7_output = A1, sensorValue = 0, MQ7_control=9;
+    const int MQ7_output = A1, MQ7_control=9;
+    int CO_value=0;
+    float MQ7_change=0;
+    enum MQ7_status {heating, cooling};
+    MQ7_status actual_status=heating;
+    const int border_ok=255, border_caution=270;
   //IR and RED measurment
     int T=20, sensorPin=A0, IR_LED=10, RED_LED=13;
     const int max_measurments_size=50, min_inserted_finger=200;
@@ -25,6 +31,8 @@
   //R and SPO2
     float REDmin=30000, REDmax=0, IRmin=30000, IRmax=0, R=0, sum_Rs=0;
     float Rs[max_bpms];
+    float k=-19, q=112;
+    const int border_ok_spo2=92, border_caution_spo2=88;
 //end constants and global variables
 
 float measure(int light_port, int input_port);
@@ -55,7 +63,10 @@ void setup() {
   lcd_backlight(neutral);
   
   digitalWrite(RED_LED,LOW);
-  digitalWrite(IR_LED,LOW);  
+  digitalWrite(IR_LED,LOW);
+  analogWrite(MQ7_control, 255);
+  MQ7_change=millis();
+  actual_status= heating;
 
   lcd.begin(16, 2);
 
@@ -64,22 +75,51 @@ void setup() {
 }
 
 void loop() {
-  float s=millis();
+  current_color=neutral;
   float IR = measure(IR_LED, sensorPin);
   float RED = measure(RED_LED, sensorPin);
-  if(IR<min_inserted_finger || RED<min_inserted_finger)
-    return;
-  write_measurment(IR, RED);
-
-  change_max_min_for_R(RED, IR);
-
-  
-  if(measurments_size==max_measurments_size)
+  if(IR>min_inserted_finger && RED>min_inserted_finger)
   {
-    compute_BPM();
-    
+    write_measurment(IR, RED);
+  
+    change_max_min_for_R(RED, IR);
+    if(measurments_size==max_measurments_size)
+      compute_BPM();
   }
-      Serial.println();
+
+  MQ7_stuff();
+
+  lcd_backlight(current_color);
+  
+  Serial.println();
+}
+
+void MQ7_stuff()
+{
+  if(millis()>MQ7_change)
+  {
+    if(actual_status==heating)
+    {
+      actual_status=cooling;
+      analogWrite(MQ7_control, 100);
+      MQ7_change=millis()+9000;
+    }
+    else if(actual_status==cooling)
+    {
+      actual_status=heating;
+      analogWrite(MQ7_control, 255);
+      CO_value=analogRead(MQ7_output); //here need to be some formula to compute
+      Serial.print(",");
+      Serial.print(CO_value);
+      MQ7_change=millis()+6000;
+    }    
+  }
+  if(CO_value<border_ok)
+    current_color=max(current_color, ok);
+  else if(CO_value>border_caution)
+    current_color=max(current_color, warning);
+  else
+    current_color=max(current_color, caution);
 }
 
 float measure(int light_port, int input_port)
@@ -151,18 +191,17 @@ float compute_BPM()
   else
     Serial.print(0);
 
-  if(sum_Rs/max_bpms>=92)
-    lcd_backlight(ok);
-  if(sum_Rs/max_bpms<=88)
-    lcd_backlight(warning);
-  else if(sum_Rs/max_bpms<92)
-    lcd_backlight(caution);
-
+  if((k*(sum_Rs/max_bpms)+q)>=border_ok_spo2)
+    current_color = ok;
+  else if((k*(sum_Rs/max_bpms)+q)<=border_caution_spo2)
+    current_color = warning;
+  else
+    current_color = caution;
 
   Serial.print(",");
   Serial.print(sum_Rs/max_bpms);
   Serial.print(",");
-  Serial.print(-19*(sum_Rs/max_bpms)+112);
+  Serial.print(k*(sum_Rs/max_bpms)+q);
   Serial.print(",");
   Serial.print(60000/(sum_bpms/max_bpms));
 }
